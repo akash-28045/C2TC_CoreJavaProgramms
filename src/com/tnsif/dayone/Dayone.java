b79c8c9a0dc63c1e2d5ffe7e9ac2d35a653842e7
akash-28045 <akashburn284@gmail.com>
2025-12-23 11:59:01 +0530

    1st commit

---
+++ src/com/tnsif/dayone/Dayone.java
@@ -0,0 +1,11 @@
+package com.tnsif.dayone;
+
+public class Dayone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world!!!");
+
+	}
+
+}
